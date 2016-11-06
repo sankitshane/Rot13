cc35a7d4d379929ef8f5d76434b251734e288716
sankit acharya <sankit.acharya@gmail.com>
2016-11-06 21:35:17 +0530

    using cookies

--- Blog/index.py
+++ Blog/index.py
@@ -19,7 +19,13 @@ class Handler(webapp2.RequestHandler):
 
 class blog(Handler):
     def get(self):
-        self.render("blog.html")
+        blogs = db.GqlQuery("SELECT * FROM new_blog ORDER BY created DESC")
+        self.render("blog.html",blogs= blogs)
+
+class new_blog(db.Model):
+    title = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
 
 class newpost(Handler):
     def render_newpost(self,title="",content="",error=""):
@@ -33,8 +39,20 @@ class newpost(Handler):
         content = self.request.get('content')
 
         if title and content:
-            self.redirect('/blog')
+            one = new_blog(title= title,content = content)
+            one.put()
+            self.redirect('/blog/%s' % str(one.key().id()))
         else:
             error = "we need both the subject and content"
             self.render_newpost(title,content,error)
+
+class Postpage(Handler):
+    def get(self,post_id):
+        key = db.Key.from_path('new_blog',int(post_id))
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+        self.render("permalink.html", post=post)
 #app = webapp2.WSGIApplication([('/',MainPage)],debug=True)
---
+++ Signup/login.py
@@ -0,0 +1,67 @@
+import webapp2
+import os
+import jinja2
+import re
+
+from google.appengine.ext import db
+
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+autoescape= True)
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class Handler(webapp2.RequestHandler):
+    def write(self,*a,**kw):
+        self.response.out.write(*a,**kw)
+    def render_str(self,template,**params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    def render(self,template,**kw):
+        self.write(self.render_str(template,**kw))
+
+class Signup(Handler):
+    def get(self):
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username = username,
+                      email = email)
+
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.response.headers.add_header('Set-Cookie','user = %s' % str(username))
+            self.redirect('/welcome')
